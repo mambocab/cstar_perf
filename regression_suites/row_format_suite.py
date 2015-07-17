89b300ec5848156f84b894f691b589378ceb063c
Jim Witschey <jim.witschey@gmail.com>
2015-07-17 13:16:57 -0400

    fixing errors in command

--- regression_suites/row_format_suite.py
+++ regression_suites/row_format_suite.py
@@ -64,8 +64,8 @@ def long_column_names_test(cluster='blade_11', load_rows='2M', write_threads=10)
     config['operations'] = [
         {'operation': 'stress',
          'command': ('user profile="https://github.com/mambocab/cstar_perf/blob/master/regression_suites/long_names.yaml" '
-                     'n={load_rows} -rate threads={write_threads} '
-                     'ops=insert\(1\)').format(load_rows=load_rows, write_threads=write_threads)},
+                     'n={load_rows} ops=insert\(1\) -rate threads={write_threads} '
+                     ).format(load_rows=load_rows, write_threads=write_threads)},
         {'operation': 'nodetool',
          'command': 'cfstats keyspace1.standard1 -H',
          'nodes': ['blade-11-2a' if cluster == 'blade_11' else 'blade-11-7a']},
